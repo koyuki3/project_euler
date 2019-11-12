5207c58a6dc4a5e873535f970840233faab74d85
koyuki3 <grand-90000@mail.ru>
2019-11-12 16:38:49 +0300

    task№1 solved

--- taxk1.cpp
+++ taxk1.cpp
@@ -1,6 +1,11 @@
 #include <iostream>
 
 int main() {
-    std::cout << "Hello, World!" << std::endl;
-    return 0;
+    int sum = 0;
+    for (int i = 0; i < 1000; ++i) {
+        if (i % 3 == 0 || i % 5 == 0) {
+            sum += i;
+        }
+    }
+    std::cout << "Sum is " << sum << std::endl;
 }
